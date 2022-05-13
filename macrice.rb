f9525b04c4fb561f8c0ff25ad55450c5d8d615dd
griffinryan <glryan@uw.edu>
2022-05-13 16:00:52 -0700

    version 0.4

---
+++ macrice.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Macrice < Formula
+  desc "Macrice is a macOS CLI tool for customization."
+  homepage "https://github.com/griffinryan/macrice"
+  url "https://github.com/griffinryan/macrice/archive/refs/tags/v0.4.tar.gz"
+  version "v0.4"
+  sha256 "31b890767c09a1fbae9fb7caee0ceaa31adb395deb2dd38e810704f7e04fe7d2"
+  license "MIT"
+
+  depends_on "cat"
+  depends_on "toilet"
+
+  def install
+    # system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "macrice"
+    bin.install Dir["lib"]
+    prefix.install "README.md"
+    prefix.install "LICENSE"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test macrice`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
