aa25a59718bb4301f89c3bb0b1b6bdc8b35e118d
griffinryan <glryan@uw.edu>
2022-05-14 00:37:16 -0700

    committed some changes to main branch!

--- macrice.rb
+++ macrice.rb
@@ -1,25 +1,24 @@
 class Macrice < Formula
-    desc "Macrice is a macOS CLI tool for customization."
-    homepage "https://github.com/griffinryan/macrice"
-    url "https://github.com/griffinryan/macrice/archive/refs/tags/0.0.4.tar.gz"
-    version "0.0.4"
-    license "MIT"
-  
-    depends_on "toilet"
-    depends_on "lolcat"
-    depends_on "boxes"
-  
-    def install
-      bin.install "macrice"
-      prefix.install Dir["lib"]
-      prefix.install Dir["fonts"]
-      prefix.install Dir["dotfiles"]
-      prefix.install "README.md"
-      prefix.install "LICENSE"
-    end
-  
-    test do
-      system "false"
-    end
+  desc "Macrice is a macOS CLI tool for customization."
+  homepage "https://github.com/griffinryan/macrice"
+  url "https://github.com/griffinryan/macrice/archive/refs/tags/0.0.5.tar.gz"
+  version "0.0.5"
+  license "MIT"
+
+  depends_on "toilet"
+  depends_on "lolcat"
+  depends_on "boxes"
+
+  def install
+    bin.install "macrice"
+    prefix.install Dir["lib"]
+    prefix.install Dir["fonts"]
+    prefix.install Dir["dotfiles"]
+    prefix.install "README.md"
+    prefix.install "LICENSE"
   end
-  
+
+  test do
+    system "false"
+  end
+end
