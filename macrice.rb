8f0de76e0467d66e75d3521faec5f255f37149a0
griffinryan <glryan@uw.edu>
2022-05-13 17:44:41 -0700

    committed some changes to main branch!

--- macrice.rb
+++ macrice.rb
@@ -4,8 +4,8 @@
 class Macrice < Formula
   desc "Macrice is a macOS CLI tool for customization."
   homepage "https://github.com/griffinryan/macrice"
-  url "https://github.com/griffinryan/macrice/archive/refs/tags/0.7.4.tar.gz"
-  version "0.7.4"
+  url "https://github.com/griffinryan/macrice/archive/refs/tags/0.7.5.tar.gz"
+  version "0.7.5"
   license "MIT"
 
   depends_on "toilet"
